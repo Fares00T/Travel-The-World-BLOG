6603efa2e5d426a48f4151a5cec007033be9b05d
Fares00T <62139647+Fares00T@users.noreply.github.com>
2022-07-02 01:23:27 +0100

    s

--- server/index.js
+++ server/index.js
@@ -19,10 +19,13 @@ app.use(cors());
 
 const URI = process.env.MONGODB_URL;
 mongoose
-  .connect("mongodb://localhost:27017", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://Fares:24244848@gmc.nanwc.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
